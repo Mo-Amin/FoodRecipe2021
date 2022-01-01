34fe3b4e73a16f2b57e514b0014ba48aed157e75
iliaspdx <ilias@pdx.edu>
2021-12-31 16:16:43 -0800

    Added Basic Modal. Added React-bootstrap

--- src/Components/MealPlan.js
+++ src/Components/MealPlan.js
@@ -1,17 +1,23 @@
 import "../App.css";
 import "bootstrap/dist/css/bootstrap.css";
+// NOT IDEAL, only import each component used, NOT the entire library. Fix this later
+//https://react-bootstrap.github.io/components/modal/ not sure why its not working
+import { Button, Modal } from 'react-bootstrap';
 import { Component } from "react";
 //import axios from "axios";
 
 import "./mealplan.css";
-let MoapiKey = "30e7963687fd483793f6d573f5db5d16";
+let MoApiKey = "30e7963687fd483793f6d573f5db5d16";
 let number = 100;
 let offset = 0;
 export default class MealPlan extends Component {
-  state = { data: [] };
+  state = { data: [], setShow: false };
+
+  handleClose = () => this.setState({setShow: false});
+  handleShow = () => this.setState({setShow: true});
 
   async componentDidMount() {
-    const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${MoapiKey}&number=${number}&offset=${offset}`;
+    const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${MoApiKey}&number=${number}&offset=${offset}`;
     const response = await fetch(url);
     const data = await response.json();
     this.setState({ data: data.results });
@@ -38,12 +44,40 @@ export default class MealPlan extends Component {
 
   render() {
     return (
-      <div className="foodimages">
-        {this.state.data.map((item, i) => (
-          <button>
-            <img alt={item.title} src={item.image} />
-          </button>
-        ))}
+      // modal below
+      //For modal I used the react bootstrap docs NOT the normal bootstrap //https://react-bootstrap.github.io/components/modal/
+      <div>
+        <Button variant="primary" onClick={this.handleShow}>
+        Filter Meals
+        </Button>
+        <Modal
+        show={this.state.setShow}
+        onHide={this.handleClose}
+        keyboard={false}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title>Modal title</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            I will not close if you click outside me. Don't even try to press
+            escape key.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={this.handleClose}>Save</Button>
+          </Modal.Footer>
+        </Modal>
+
+        {/* //index.js:1 Warning: Each child in a list should have a unique "key" prop. */}
+        <div className="foodimages">
+          {this.state.data.map((item, i) => (
+            <button>
+              <img alt={item.title} src={item.image} />
+            </button>
+          ))}
+        </div>
       </div>
     );
   }
